435d3c2e3e9cb0aaf7160cdc10f0ef47cdd24906
KhinThandarTun <106916866+KhinThandarTun@users.noreply.github.com>
2023-11-03 17:22:20 +0630

    Update

---
+++ UniqueStandardProject/Model/PlayerModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniqueStandardProject.Model
+{
+    public class PlayerModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ UniqueStandardProject/Pages/Detail.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using UniqueStandardProject.Model;
+
+namespace UniqueStandardProject.Pages
+{
+    public class DetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
