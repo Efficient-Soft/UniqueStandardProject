8ea51cd756c82ec8e32808bb96dd6ba48472c3b5
KhinThandarTun <106916866+KhinThandarTun@users.noreply.github.com>
2023-11-06 17:23:53 +0630

    Update

--- UniqueStandardProject/Pages/Detail.cshtml.cs
+++ UniqueStandardProject/Pages/Detail.cshtml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,8 +11,13 @@ namespace UniqueStandardProject.Pages
 {
     public class DetailModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet(string title, string description)
         {
+            string desc = Regex.Match(description, @"<strong>(.*?)</strong>").Groups[1].Value;
+            ViewData["title"] = title;
+            ViewData["description"] = desc; 
+
+            return Page();
         }
     }
 }
