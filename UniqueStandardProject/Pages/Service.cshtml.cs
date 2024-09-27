b55bc1af08edad4323dc9fcd20eb9578bdafd9e6
Khin Thandar Tun <106916866+KhinThandarTun@users.noreply.github.com>
2024-09-27 11:13:10 +0630

    Update Service

--- UniqueStandardProject/Pages/Service.cshtml.cs
+++ UniqueStandardProject/Pages/Service.cshtml.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using UniqueStandardProject.Entities;
 
 namespace UniqueStandardProject.Pages
 {
     public class ServiceModel : PageModel
     {
-        public void OnGet()
+        public void OnGet(string serviceTitle)
         {
+            ViewData["ServiceTitle"] = serviceTitle;
         }
     }
 }
