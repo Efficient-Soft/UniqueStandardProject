54af598dbbef2650c4d8f67c20734b84ca095c98
Khin Thandar Tun <106916866+KhinThandarTun@users.noreply.github.com>
2024-11-26 11:28:32 +0630

    Create RoundTheCodeFileLoggerOptions.cs

---
+++ UniqueStandardProject/LoggingFile/RoundTheCodeFileLoggerOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace UniqueStandardProject.LoggingFile
+{
+    public class RoundTheCodeFileLoggerOptions
+    {
+        public virtual string FilePath { get; set; }
+
+        public virtual string FolderPath { get; set; }
+    }
+}
