3166e648523eac2fa139924889b98e8682c65fde
Yell Min Naing <yellminnaing5@gmail.com>
2026-08-25 15:55:44 +0630

    Update Create.cshtml.cs

--- UniqueStandardProject/Areas/Products/Pages/WeldingTraining/Create.cshtml.cs
+++ UniqueStandardProject/Areas/Products/Pages/WeldingTraining/Create.cshtml.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Threading.Tasks;
 using UniqueStandardProject.Data;
 using UniqueStandardProject.Interfaces;
+using UniqueStandardProject.Services;
 
 namespace UniqueStandardProject.Areas.Products.Pages.WeldingTraining
 {
@@ -53,7 +54,11 @@ namespace UniqueStandardProject.Areas.Products.Pages.WeldingTraining
             {
                 if (!string.IsNullOrEmpty(Base64String_Photo))
                 {
-                    byte[] bytes = Convert.FromBase64String(Base64String_Photo.Replace("data:image/jpeg;base64,", string.Empty));
+                    if (!ImageDataUrlParser.TryParse(Base64String_Photo, out byte[] bytes))
+                    {
+                        ModelState.AddModelError("", "The selected image is invalid. Please choose another image.");
+                        return Page();
+                    }
                     MemoryStream ms = new(bytes);
                     Image image = Image.FromStream(ms);
                     ms.Close();
