bf860ef767bed11d2509ff362a790c91d9366dd3
Yell Min Naing <yellminnaing5@gmail.com>
2026-08-25 15:24:22 +0630

    Update Dashboard.cshtml.cs

--- UniqueStandardProject/Pages/Dashboard.cshtml.cs
+++ UniqueStandardProject/Pages/Dashboard.cshtml.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 
 namespace UniqueStandardProject.Pages
 {
+    [Authorize]
     public class DashboardModel : PageModel
     {
         public void OnGet()
