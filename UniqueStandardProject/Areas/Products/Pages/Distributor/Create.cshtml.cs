f6ee4caad68c0e0765499d0932aded340355615e
Yell Min Naing <yellminnaing5@gmail.com>
2026-08-25 15:55:38 +0630

    Update Create.cshtml.cs

--- UniqueStandardProject/Areas/Products/Pages/Distributor/Create.cshtml.cs
+++ UniqueStandardProject/Areas/Products/Pages/Distributor/Create.cshtml.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using UniqueStandardProject.Data;
 using UniqueStandardProject.Interfaces;
+using UniqueStandardProject.Services;
 
 namespace UniqueStandardProject.Areas.Products.Pages.Distributor
 {
@@ -66,7 +67,11 @@ namespace UniqueStandardProject.Areas.Products.Pages.Distributor
             {
                 if (!string.IsNullOrEmpty(Base64String_Photo))
                 {
-                    byte[] bytes = Convert.FromBase64String(Base64String_Photo.Replace("data:image/jpeg;base64,", string.Empty));
+                    if (!ImageDataUrlParser.TryParse(Base64String_Photo, out byte[] bytes))
+                    {
+                        ModelState.AddModelError("", "The selected image is invalid. Please choose another image.");
+                        return Page();
+                    }
                     MemoryStream ms = new(bytes);
                     Image image = Image.FromStream(ms);
                     ms.Close();
